2cfd9f8b2aa5bff0ae4d762f27f4a605c6ed01a6
Michael Macias <zaeleus@gmail.com>
2019-08-09 19:46:26 -0500

    lib: Change module visibility

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,8 @@
 pub use self::bloom_filter::BloomFilter;
 pub use self::scalable_bloom_filter::ScalableBloomFilter;
 
-pub mod bloom_filter;
-pub mod double_hasher;
-pub mod scalable_bloom_filter;
+mod bloom_filter;
+mod double_hasher;
+mod scalable_bloom_filter;
 
-pub type DefaultHashBuilder = std::collections::hash_map::RandomState;
+type DefaultHashBuilder = std::collections::hash_map::RandomState;
